aeac4db35fd41431c73164abdcf981e2327f2042
студент <@polytech.edu>
2022-10-17 13:27:58 +0300

    Добавьте файлы проекта.

---
+++ App.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace gdm_n0cl1p_check_ml9_th1s_m0m3nt_d3aD {
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application {
+    }
+}
